4721c2f9128b4845b95f9505003db91c73058ddc
Georg Sieber <it@georg-sieber.de>
2023-06-29 14:34:18 +0200

    add serial barcode scanner implementation

---
+++ bookscanner.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import configparser
+import serial
+import time
+import os
+
+
+# read config
+config = {}
+configParser = configparser.ConfigParser()
+configParser.read(str(Path.home())+'/.config/staffworkstation4linux.ini')
+if(configParser.has_section('scanner')): config = dict(configParser.items('scanner'))
+
+# open serial port
+ser = serial.Serial(
+    config.get('port', '/dev/ttyS0'),
+    int(config.get('rate', 9600)),
+)
+print('Connected to:', ser.name)
+
+# read barcodes from scanner and send to OS
+requiredGoodReads = int(config.get('requiredgoodreads', 3))
+currentGoodReads = 0
+currentBarcode = ''
+while True:
+    barcode = ser.readline().decode('ascii').strip()
+    if(barcode == currentBarcode):
+        currentGoodReads += 1
+    else:
+        currentGoodReads = 0
+    currentBarcode = barcode
+
+    if(currentGoodReads == requiredGoodReads):
+        print('Barcode:', barcode)
+        try:
+            import pyautogui
+            pyautogui.typewrite(barcode)
+            pyautogui.press('enter')
+        except Exception as e:
+            print('Unable to send keystrokes to desktop:', type(e), e)
+
+    time.sleep(0.01)
