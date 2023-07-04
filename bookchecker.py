384ae57714f2d3a8b64baf0edc6b82b216382f54
Georg Sieber <it@georg-sieber.de>
2023-07-04 15:10:26 +0200

    add auto/manual mode and verifier light features

--- bookchecker.py
+++ bookchecker.py
@@ -32,7 +32,9 @@ dev.set_configuration()
 def getStatusText1(status1):
     return (
         ('check-in' if status1[0] else 'check-out') +', '+
-        ('normal-mode' if status1[1] else 'magnetic-media-mode')
+        ('normal-mode' if status1[1] else 'magnetic-media-mode') +', '+
+        ('verifier-on' if status1[2] else 'verifier-off') +', '+
+        ('manual' if status1[3] else 'auto')
     )
 def getStatusText2(status2):
     verifierIndicator = status2[7] & 0b00001000
@@ -45,15 +47,21 @@ def getStatusText2(status2):
 status1    = dev.ctrl_transfer(0xC0, 0xbd, 0xffff, 0xffff, 9)
 bInOut     = status1[0]
 bMediaMode = status1[1]
+bAutoMode  = status1[2]
+bVerifierLight = status1[3]
 print('status 1:', bytes(status1).hex(), '('+getStatusText1(status1)+')')
 
 status2 = dev.ctrl_transfer(0xC0, 0xb7, 0x0000, 0x0000, 9)
 print('status 2:', bytes(status2).hex(), '('+getStatusText2(status2)+')')
 
 # set status
-def sendStatusUpdate(bInOut, bMediaMode):
-    status = (bMediaMode << 8) | bInOut
-    statusResponse = dev.ctrl_transfer(0xC0, 0xbd, status, 0x01ff, 9)
+def sendStatusUpdate(bInOut, bMediaMode, bAutoMode=0xff, bVerifierLight=0xff):
+    # 0xff = no change
+    # 0x01 = enable feature
+    # 0x00 = disable feature
+    status1 = (bMediaMode << 8) | bInOut
+    status2 = (bAutoMode << 8) | bVerifierLight
+    statusResponse = dev.ctrl_transfer(0xC0, 0xbd, status1, status2, 9)
     print('set resp:', bytes(statusResponse).hex(), '('+getStatusText1(statusResponse)+')')
 
 changed = False
@@ -61,7 +69,7 @@ auto    = False
 for arg in sys.argv:
     if arg == sys.argv[0]: continue
 
-    if arg == 'a':
+    if arg == 'background':
         auto = True
 
     if arg == 'i':
@@ -78,9 +86,23 @@ for arg in sys.argv:
         bMediaMode = 0x00
         changed = True
 
+    if arg == 'a':
+        bAutoMode = 0x00
+        changed = True
+    if arg == 'm':
+        bAutoMode = 0x01
+        changed = True
+
+    if arg == 'l':
+        bVerifierLight = 0x01
+        changed = True
+    if arg == 'k':
+        bVerifierLight = 0x00
+        changed = True
+
 # set mode as given by command line parameter
 if changed:
-    sendStatusUpdate(bInOut, bMediaMode)
+    sendStatusUpdate(bInOut, bMediaMode, bAutoMode, bVerifierLight)
 
 # setup window title listener for automatic mode change based on open windows
 if auto:
